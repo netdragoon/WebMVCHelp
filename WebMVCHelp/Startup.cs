e7c0aab2fa51128ab0370b72d33061ec3b605bd4
netdragoon <netdragoon@outlook.com>
2015-10-30 20:58:50 -0200

    MVC ASP.NET 4.5

---
+++ WebMVCHelp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebMVCHelp.Startup))]
+namespace WebMVCHelp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
