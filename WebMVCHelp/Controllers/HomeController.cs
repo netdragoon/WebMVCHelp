683ea9542249734c4248ca3f455832095934e0d8
netdragoon <netdragoon@outlook.com>
2015-10-30 22:51:34 -0200

    Commit Drop

--- WebMVCHelp/Controllers/HomeController.cs
+++ WebMVCHelp/Controllers/HomeController.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMVCHelp.DAL.Interfaces;
 
 namespace WebMVCHelp.Controllers
 {
     public class HomeController : Controller
     {
+        public IDalCredit DalCredit;
+        public HomeController(IDalCredit DalCredit)
+        {
+            this.DalCredit = DalCredit;
+        }
         public ActionResult Index()
         {
+            //Primeira forma (muito usado essa)
+            ViewBag.Drop1 = new SelectList(DalCredit.All(), "CreditId", "Description");
+            //Segunda Forma posicionando o item
+            //3 é aonde está gravado
+            ViewBag.Drop2 = new SelectList(DalCredit.All(), "CreditId", "Description", 3);
+            //Terceira Forma
+            ViewBag.Drop3 = DalCredit.All();
+
             return View();
         }
 
