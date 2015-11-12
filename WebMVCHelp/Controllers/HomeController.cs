5fbf32c85dfe5766e70cddc32532b08feeccb63d
netdragoon <netdragoon@outlook.com>
2015-11-12 12:33:35 -0200

    aLL

--- WebMVCHelp/Controllers/HomeController.cs
+++ WebMVCHelp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebMVCHelp.DAL.Interfaces;
+using WebMVCHelp.Models;
 
 namespace WebMVCHelp.Controllers
 {
@@ -36,9 +37,53 @@ namespace WebMVCHelp.Controllers
 
         public ActionResult Contact()
         {
+            DateTime date = DateTime.Now;
+            var c = date.ToString("dd/MM/yyyy");
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult PostItems(HttpPostedFileBase file)
+        {
+
+            long length = file.InputStream.Length;
+
+            byte[] arrayByteFile = new byte[length];
+
+            file.InputStream.Read(arrayByteFile, 0, (int)length);
+
+            //na variavel arrayByteFile você tem o array de byte para gravar no seu banco
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet()]
+        public ActionResult Sexo()
+        {            
+            return View();
+        }
+
+        [HttpPost()]
+        public ActionResult Sexo(Peoples people)
+        {
+            Peoples p = people;
+            return View();
+        }
+
+        [HttpPost()]
+        public JsonResult GetSexo()
+        {
+
+            IList<Sexo> SexoList = new List<Sexo>()
+            {
+                new Sexo() { Id = 1, Description = "MASCULINO" },
+                new Sexo() { Id = 2, Description = "FEMININO" }
+            };
+
+            return Json(SexoList, JsonRequestBehavior.DenyGet);
+
         }
     }
 }
