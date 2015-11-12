26566c35d7d416e795a0524f85afbb018bb581e1
netdragoon <netdragoon@outlook.com>
2015-11-12 12:36:53 -0200

    commit

---
+++ WebMVCHelp.Models/Peoples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebMVCHelp.Models
+{
+    public class Peoples
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SexoId { get; set; }
+        public Sexo Sexo { get; set; }
+    }
+}
